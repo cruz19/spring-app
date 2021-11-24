61f3c07fb070bc80fb558679043168e8271867a2
Yhon Steven Cruz <blackjackers17@gmail.com>
2021-11-24 11:02:49 -0500

    Prueba 18

--- src/main/java/cundi/edu/co/demo/controller/EditorialController.java
+++ src/main/java/cundi/edu/co/demo/controller/EditorialController.java
@@ -7,6 +7,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -24,6 +25,7 @@ import cundi.edu.co.demo.exception.ModelNotFoundException;
 import cundi.edu.co.demo.service.IAutorEditorialService;
 import cundi.edu.co.demo.service.IEditorialService;
 
+@PreAuthorize("hasAuthority('Vendedor') ")
 @RestController
 @RequestMapping("/editoriales")
 public class EditorialController {
